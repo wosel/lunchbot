ed5f045967a35cea9a89d8755e7b67934285fa14
Jakub Hajic <Jakub.Hajic@goodvisionlive.com>
2019-09-10 09:03:39 +0200

    Initial commit

---
+++ lunchbot.py
@@ -0,0 +1,130 @@
+import discord
+import datetime
+import time
+from pubs import *
+
+TOKEN = 'NjE5NDMxMzgyMzAwNDkxNzg4.XXas5w.oYH3c7qCjUvI0xrV_so-OyJRvZ8'
+
+client = discord.Client()
+last_mess = datetime.datetime.now() - datetime.timedelta(seconds=10)
+cache = {}
+
+def all_upper(inp_string):
+    return all(x.isupper() for x in inp_string.replace(',', '').replace('.', '').replace(' ', '').replace(':', '').replace(';', ''))
+
+def translate_string(inp_string):
+    r = requests.post('https://lindat.mff.cuni.cz/services/translation/api/v2/models/cs-en', {'input_text': inp_string} )
+    dec = r.content.decode()
+    ev = eval(dec)
+    tx = ev[0].strip()
+    return tx
+
+def translate_msg(msg_cz):
+    msg_split = msg_cz.split('\n')
+    msg_en_split = [msg_split[0]]
+    for mline in msg_split[1:]:
+        l_split = mline.split(' ')
+        new_line = []
+        first = True
+        for word in l_split:
+            new_word = word
+            if len(word) > 0 and  all_upper(word):
+                lowerword = word.lower()
+                if first:
+                    first = False
+                    new_word = lowerword[0].upper() + lowerword[1:]
+                else:
+                    new_word = lowerword
+
+            new_line.append(new_word)
+
+        fixed_line = ' '.join(new_line)
+        translated_line = ''
+        if len(fixed_line.strip()) > 0:
+            translated_line = translate_string(fixed_line)
+        msg_en_split.append(translated_line)
+
+    msg_en = '\n'.join(msg_en_split)
+    return msg_en
+
+def write_pub(pub, cache, cur_day, fp, lang='cz'):
+    if pub in cache and cache[pub]['day'] == cur_day:
+        if lang == 'cz':
+            msg_cz = cache[pub]['msg_cz']
+            return msg_cz
+        if lang == 'en':
+            if 'msg_en' in cache[pub]:
+                msg_en = cache[pub]['msg_en']
+                return msg_en
+            else:
+                msg_cz = cache[pub]['msg_cz']
+                msg_en = translate_msg(msg_cz)
+                cache[pub]['msg_en'] = msg_en
+                return msg_en
+    else:
+        msg_cz = fp[pub]()
+        cache[pub] = {}
+        if lang == 'cz':
+            cache[pub]['msg_cz'] = msg_cz
+            cache[pub]['day'] = cur_day
+            return msg_cz
+        if lang == 'en':
+            msg_en = translate_msg(msg_cz)
+            cache[pub]['msg_cz'] = msg_cz
+            cache[pub]['msg_en'] = msg_en
+            cache[pub]['day'] = cur_day
+            return msg_en
+
+@client.event
+async def on_message(message):
+    global cache
+    global last_mess
+    fp = {'klid': klid, 'upecku': upecku, 'peprasul': peprasul, 'naradnici': naradnici}
+    cur_day = datetime.datetime.now().day
+
+    # we do not want the bot to reply to itself
+    delta = datetime.timedelta(seconds=10)
+    if message.author == client.user:
+        return
+    mt = message.timestamp
+    mtm, mts = mt.minute, mt.second
+    lim = last_mess + delta
+    lm, ls = lim.minute, lim.second
+
+    if (mtm == lm and mts < ls) or mtm < lm:
+        print('too soon')
+        return
+    else:
+        print('working on {}'.format(message.content))
+    for pub in fp.keys():
+        if message.content.startswith('!'+pub):
+            msg = write_pub(pub, cache, cur_day, fp, 'cz')
+            await client.send_message(message.channel, msg)
+            last_mess = datetime.datetime.now()
+
+        elif message.content.startswith('!en_'+pub):
+            msg = write_pub(pub, cache, cur_day, fp, 'en')
+            await client.send_message(message.channel, msg)
+            last_mess = datetime.datetime.now()
+
+    if message.content.startswith('!all'):
+        for pub in fp.keys():
+            msg = write_pub(pub, cache, cur_day, fp, 'cz')
+            await client.send_message(message.channel, msg)
+        last_mess = datetime.datetime.now()
+
+    if message.content.startswith('!en_all'):
+        for pub in fp.keys():
+            msg = write_pub(pub, cache, cur_day, fp, 'en')
+            await client.send_message(message.channel, msg)
+        last_mess = datetime.datetime.now()
+
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
---
+++ pubs.py
@@ -0,0 +1,122 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+
+def klid():
+
+    response = requests.get('http://www.klidpopraci.cz/tydenni-menu/')
+
+    soup = BeautifulSoup(response.content, 'lxml')
+    retstr = 'KLID\n'
+    day_ctr = 0
+    today = datetime.datetime.today().weekday()
+    for header in soup.find_all('h2'):
+        htext = header.get_text().strip()
+        if htext == '':
+            continue
+        if today != day_ctr:
+            day_ctr += 1
+            continue
+        last_txt = ''
+        for elem in header.next_siblings:
+            try:
+                txt = elem.get_text().strip().replace('\n', ' ')
+            except:
+                pass
+            if elem.name and elem.name.startswith('h') and txt != '':
+                break
+            if elem.name == 'p' and txt != '':
+                if txt[0] != txt.lower()[0] and last_txt != '':
+                    retstr += last_txt + '\n'
+                    last_txt = txt
+                else:
+                    last_txt = last_txt +' '+ txt
+
+            if elem.name == 'div':
+                for el in elem.findChildren('p'):
+                    eltxt = el.get_text().strip().replace('\n', ' ')
+                    if eltxt != '':
+                        if eltxt[0] != eltxt.lower()[0] and last_txt != '':
+                            retstr += last_txt + '\n'
+                            last_txt = eltxt
+                        else:
+                            last_txt = last_txt +' '+ eltxt
+
+        retstr += last_txt + '\n'
+        break
+    return retstr
+
+def peprasul():
+    response = requests.get('https://peprasul.cz/poledni-menu/')
+    soup = BeautifulSoup(response.content, 'lxml')
+
+
+    body = soup.find_all('body')[0]
+
+    mark = False
+    divcl = ''
+    for s in body.findChildren():
+        if mark:
+            if s.name == 'div':
+                cl=s['class']
+                cl0 = cl[0]
+                if cl0.startswith('cz_') and cl0[3] in '0123456789':
+                    divcl = cl0
+                    break
+        if s.get_text().strip().startswith('HLAV'):
+
+            mark = True
+    menudiv = soup.find_all('div', divcl)
+    retstr = 'PEPŘ A SŮL\n'
+
+    for food in menudiv[0].findChildren('span'):
+
+        el = food.findChildren('b')
+        if len(el) > 0:
+            tx = el[0].get_text().strip()
+            retstr += tx + '\n'
+    return retstr
+
+def upecku():
+
+    response = requests.get('http://upecku.cz/menu/')
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    retstr = 'U PECKŮ\n'
+    daily = soup.findAll('div', {'id': 'tabs-1'})
+
+    for menuitem in daily[0].findChildren('div', {'class': 'menu-item'}):
+        for span in menuitem.findChildren('span'):
+
+            txt = span.get_text().strip()
+            if len(txt) > 4:
+                retstr += txt + '\n'
+    return retstr
+
+def naradnici():
+    response = requests.get('http://www.hospudkanaradnici.cz/new2016-2/index.php/menu/denni-menu')
+    soup = BeautifulSoup(response.content, 'lxml')
+
+    retstr = 'NA RADNICI\n'
+    iframe = soup.find_all('iframe')[0]
+    response = requests.get(iframe.attrs['src'])
+    iframe_soup = BeautifulSoup(response.content, 'lxml')
+    cont_div = iframe_soup.findAll('div', {'id': 'contents'})[0]
+    last = ''
+    for p in cont_div.findChildren('p'):
+        txt = p.get_text().strip().replace('.', '').replace('…', ' ')
+        txt = ' '.join(txt.split())
+        if txt == 'MOUČNÍKY':
+            break
+        if len(txt) < 1 or txt == 'POLÉVKY' or txt == 'NABÍDKA DNE':
+            continue
+        if txt[0].lower() == txt[0] and txt[0] != '¼' :
+            last += ' ' + txt
+        else:
+            retstr += last + '\n'
+            last = txt
+    return retstr
+
+
+
+
